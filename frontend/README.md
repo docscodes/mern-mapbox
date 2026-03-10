d7da73a865e358e34c9d973afe4bf470ac96fb62
docscodes <docscodes@gmail.com>
2026-03-10 14:02:01 -0400

    install mapbox

--- frontend/README.md
+++ frontend/README.md
@@ -1,3 +1,5 @@
 ```
 npx create-react-app .
+
+npm install react-map-gl mapbox-gl @types/mapbox-gl
 ```
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,29 @@
+import "mapbox-gl/dist/mapbox-gl.css";
+import React from "react";
+import Map, { Marker } from "react-map-gl/mapbox";
+
 function App() {
-  return <div className="App">hello</div>;
+  const [viewState, setViewState] = React.useState({
+    latitude: 46,
+    longitude: 17,
+    zoom: 4,
+  });
+
+  return (
+    <Map
+      {...viewState}
+      onMove={(evt) => setViewState(evt.viewState)}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      style={{ width: "100vw", height: "100vh" }}
+    >
+      <Marker
+        latitude={48.858093}
+        longitude={2.294694}
+        color="red"
+      />
+    </Map>
+  );
 }
 
 export default App;
