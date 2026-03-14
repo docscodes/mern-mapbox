1879c9b2223e05d552f0390ccceb59284a6bc155
docscodes <docscodes@gmail.com>
2026-03-13 23:37:28 -0400

    add popup for markers

--- frontend/README.md
+++ frontend/README.md
@@ -2,4 +2,7 @@
 npx create-react-app .
 
 npm install react-map-gl mapbox-gl @types/mapbox-gl
+
+npm install @mui/material @emotion/react @emotion/styled
+npm install @mui/icons-material
 ```
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
+import { Star } from "@mui/icons-material";
 import "mapbox-gl/dist/mapbox-gl.css";
 import React from "react";
-import Map, { Marker } from "react-map-gl/mapbox";
+import Map, { Marker, Popup } from "react-map-gl/mapbox";
+import "./app.css";
 
 function App() {
   const [viewState, setViewState] = React.useState({
@@ -13,7 +15,7 @@ function App() {
     <Map
       {...viewState}
       onMove={(evt) => setViewState(evt.viewState)}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapStyle="mapbox://styles/mapbox/streets-v12"
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
       style={{ width: "100vw", height: "100vh" }}
     >
@@ -22,6 +24,32 @@ function App() {
         longitude={2.294694}
         color="red"
       />
+      <Popup
+        latitude={48.858093}
+        longitude={2.294694}
+        closeButton={true}
+        closeOnClick={false}
+        anchor="left"
+      >
+        <div className="card">
+          <label>Place</label>
+          <h4 className="place">Eiffell Tower</h4>
+          <label>Review</label>
+          <p className="desc">Beautiful place. I like it.</p>
+          <label>Rating</label>
+          <div className="stars">
+            <Star className="star" />
+            <Star className="star" />
+            <Star className="star" />
+            <Star className="star" />
+          </div>
+          <label>Information</label>
+          <span className="username">
+            Created by <b>John Doe</b>
+          </span>
+          <span className="date">1 hour ago</span>
+        </div>
+      </Popup>
     </Map>
   );
 }
