423d8605d00e6bcfec2002c2d1517f2278e8a22b
docscodes <docscodes@gmail.com>
2026-03-14 21:24:08 -0400

    wait for connection

--- backend/index.js
+++ backend/index.js
@@ -10,20 +10,17 @@ dotenv.config();
 
 const app = express();
 
-try {
-  mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log("MongoDB connected");
-  });
-} catch (error) {
-  handleError(error);
-}
-
 app.use(express.json());
 app.use("/api/pins", pinRoute);
 app.use("/api/users", userRoute);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("MongoDB connected");
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}).catch((err) => {
+  console.error("MongoDB connection error:", err);
 });
 
 module.exports = app;
